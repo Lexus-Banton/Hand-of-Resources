0d1bacfa3318092dae1dd06df40a1647b087ddec
Lexus Banton <lexusbanton.kr@gmail.com>
2022-11-15 19:17:39 -0800

    built resturants data set

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,26 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS resturants;
+
+CREATE TABLE resturants (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  type VARCHAR NOT NULL,
+  cuisine VARCHAR NOT NULL
+);
+
+INSERT INTO resturants(
+    name,
+    type,
+    cuisine
+)
+
+VALUES 
+('In-N-Out', 'Fast-Food', 'American'),
+('Mazzellas', 'Sit-Down', 'Italian'),
+('Mikimotos', 'Sit-Down', 'Japanese'),
+('MAXXS', 'Fast-Food', 'American'),
+('D&H', 'Sit-Down', 'Jamaican');
+ 
+
+
